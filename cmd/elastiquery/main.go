8b9bc5356194016f7e154f33bc03d2868edf255b
Adam Wolfe Gordon <awg@xvx.ca>
2018-04-06 13:53:55 -0600

    Add support for range queries and date ranges

--- cmd/elastiquery/main.go
+++ cmd/elastiquery/main.go
@@ -34,6 +34,11 @@ func main() {
 	rawQuery := flag.String("raw", "", "Raw ElasticSearch JSON query")
 	termQueries := flag.String("terms", "", "Semicolon-separated ElasticSearch term queries in the form field=term")
 	prefixQueries := flag.String("prefixes", "", "Semicolon-separated ElasticSearch term queries in the form field=term")
+	rangeQueries := flag.String("ranges", "", "Semi-colon-separated ElasticSearch range queries in the form field=from~to")
+
+	timeField := flag.String("time-field", "@timestamp", "Field name for record timestamps")
+	timeFrom := flag.String("time-start", "", "Start time for search, in RFC3339 format")
+	timeTo := flag.String("time-end", "", "End time for search, in RFC3339 format")
 
 	envflag.Parse()
 	flag.Parse()
@@ -86,22 +91,62 @@ func main() {
 			queries = append(queries, client.PrefixQuery(parts[0], parts[1]))
 		}
 	}
+	if *rangeQueries != "" {
+		qs := strings.Split(*rangeQueries, ";")
+		for _, q := range qs {
+			parts := strings.SplitN(q, "=", 2)
+			if len(parts) != 2 {
+				log.Fatalf("Invalid range query %q", q)
+			}
+			fromto := strings.SplitN(parts[1], "~", 2)
+			if len(fromto) != 2 {
+				log.Fatalf("Invalid range %q", fromto)
+			}
+			queries = append(queries, client.RangeQuery(parts[0], fromto[0], fromto[1]))
+		}
+	}
 
 	if len(queries) == 0 {
 		log.Fatal("No queries specified")
+	}
+
+	var timeRangeQuery elastiquery.ESQuery
+	if *timeFrom != "" || *timeTo != "" {
+		// Use interface{} here so that we get nil for unspecified start or end.
+		var from, to interface{}
+
+		if *timeFrom != "" {
+			from, err = time.Parse(time.RFC3339, *timeFrom)
+			if err != nil {
+				log.Fatalf("Invalid time %q", *timeFrom)
+			}
+		}
+		if *timeTo != "" {
+			to, err = time.Parse(time.RFC3339, *timeTo)
+			if err != nil {
+				log.Fatalf("Invalid time %q", *timeTo)
+			}
+		}
+		timeRangeQuery = client.RangeQuery(*timeField, from, to)
+	}
+
+	var compositeQuery elastiquery.ESQuery
+	if len(queries) == 1 {
+		compositeQuery = queries[0]
+	} else if *or {
+		compositeQuery = client.OrQuery(queries...)
+	} else {
+		compositeQuery = client.AndQuery(queries...)
+	}
+	if timeRangeQuery != nil {
+		compositeQuery = client.AndQuery(compositeQuery, timeRangeQuery)
 	}
 
 	opts := parseQueryOpts(*offset, *limit, *sortField, *sortReverse)
 	ctx, cancel := context.WithTimeout(context.Background(), *timeout)
 	defer cancel()
 	var result elastiquery.ESResult
-	if len(queries) == 1 {
-		result, err = queries[0].Do(ctx, esIndex, opts...)
-	} else if *or {
-		result, err = client.OrQuery(queries...).Do(ctx, esIndex, opts...)
-	} else {
-		result, err = client.AndQuery(queries...).Do(ctx, esIndex, opts...)
-	}
+	result, err = compositeQuery.Do(ctx, esIndex, opts...)
 
 	if err != nil {
 		log.Fatalf("Query failed: %v", err)
